45cf80da4db042f009ed45ea3e6ee58a94ad73a3
georgekovin <108282258+georgekovin@users.noreply.github.com>
2023-12-20 20:52:55 +0300

    model built

--- model/motorica_pl.py
+++ model/motorica_pl.py
@@ -1,17 +1,43 @@
 import numpy as np
 import pandas as pd
-import joblib as jl
+import pickle
+from sklearn.base import BaseEstimator
 
 
-class MotoricaPipeline:
+class MotoricaPipeline(BaseEstimator):
     def __init__(self) -> None:
-        pass
+        """Пайплайн для предсказания жеста человека по датчикам на его руке."""
+        
+        with open('./motorica_pl.pkl', 'rb') as pl_file:
+            self.pipeline = pickle.load(pl_file)
     
     def fit(self, X):
-        self.pipeline = jl.load('motorica_pl.pkl')
+        """Обучение пайплайна 
+        (на самом деле он уже обученный, данный метод - просто формальность)
+
+        Args:
+        ---
+            X (`ArrayLike`): трехмерный массив с размерностью (`наблюдения`, `датчики`, `время`)
+
+        Returns:
+        ---
+            `self`: обученный пайплайн
+        """
         return self 
     
     def predict(self, X, as_df=False):
+        """Предсказание жестов по датчикам на руке.
+
+        Args:
+        ---
+            X (`ArrayLike`): трехмерный массив с размерностью (`наблюдения`, `датчики`, `время`)
+            as_df (bool, optional): при `True` метод возвращает результат в формате DataFrame, по умолчанию - `False`
+
+        Returns:
+        ---
+            `DataFrame` | `NDArray`: двумерный массив с закодированными жестами с размерностью (`наблюдения`, `время`)
+        """
+        
         y = np.split(self.pipeline.predict(X), X.shape[0])
     
         if as_df:
@@ -25,6 +51,16 @@ class MotoricaPipeline:
         return np.array(y) 
     
     def fit_predict(self, X, as_df=False):
+        """Обучение пайплайна и затем предсказание жестов.
+
+        Args:
+        ---
+            X (`ArrayLike`): трехмерный массив с размерностью (`наблюдения`, `датчики`, `время`)
+            as_df (bool, optional): при `True` метод возвращает результат в формате DataFrame, по умолчанию - `False`
+
+        Returns:
+        ---
+            `DataFrame` | `NDArray`: двумерный массив с закодированными жестами с размерностью (`наблюдения`, `время`)
+        """
+        
         return self.fit(X).predict(X, as_df=as_df)
-
-
